1d4cd62cd9f17b6e4cb3f1454d13e07cf7686991
hoangphuc <hoangphucnb97@gmail.com>
2023-05-29 14:54:43 +0700

    First script to mint mpx token

---
+++ scripts/mint_mpx.ts
@@ -0,0 +1,30 @@
+import { readMPXValidators } from "./utils.ts";
+
+import { Lucid, Blockfrost, Data } from "https://deno.land/x/lucid@0.10.1/mod.ts";
+import { fromText } from "https://deno.land/x/lucid@0.10.1/mod.ts";
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
+
+
+const blockfrostKey = Deno.env.get("BLOCKFROST_KEY");
+
+const lucid = await Lucid.new(
+    new Blockfrost("https://cardano-preview.blockfrost.io/api/v0", blockfrostKey),
+    "Preview",
+);
+
+lucid.selectWalletFromPrivateKey(await Deno.readTextFile("./owner.sk"));
+
+const mpxValidators = readMPXValidators(lucid);
+
+const assetName = `${mpxValidators.mpxPolicyId}${fromText("MPX")}`;
+
+
+const tx = await lucid.newTx()
+    .attachMintingPolicy(mpxValidators.mpxPolicy)
+    .mintAssets({ [assetName]: BigInt(10) }, Data.void())
+    .complete();
+
+const txSigned = await tx.sign().complete();
+const txHash = await txSigned.submit();
+await lucid.awaitTx(txHash);
+console.log(`Mint sucess at tx ${txHash}`);
