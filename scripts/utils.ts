27446c1f7f927fa29c9bd9efa8d95988175c50de
hoangphuc <hoangphucnb97@gmail.com>
2023-05-29 15:47:28 +0700

    Check address balance before transfer

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,6 +4,7 @@ import {
     Lucid,
 } from "https://deno.land/x/lucid@0.10.1/mod.ts";
 import { MPXValidators } from "./types.ts";
+import { BlockFrostAPI } from "npm:@blockfrost/blockfrost-js";
 export function readMPXValidators(lucid: Lucid): MPXValidators {
     const mpx = blueprint.validators.find((v) => v.title === "mpx_token.mpx_token");
     if (!mpx) {
@@ -18,5 +19,14 @@ export function readMPXValidators(lucid: Lucid): MPXValidators {
             type: 'PlutusV2',
             script: mpx.compiledCode,
         }
+    }
+}
+
+export async function readAssetOfAddress(api: BlockFrostAPI, userAddress: string, assetName = 'lovelace'): Promise<{ unit: string, quantity: string }> {
+    const userInfo = await api.addresses(userAddress);
+    const userAsset = userInfo.amount.filter((asset) => asset.unit === assetName)[0];
+    return {
+        unit: assetName,
+        quantity: userAsset?.quantity || '0',
     }
 }
